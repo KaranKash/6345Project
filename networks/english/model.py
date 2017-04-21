bcd089b3ae32f617bab409f1edd3cf65445c88ed
Karan Kashyap <kashkar@mit.edu>
2017-04-21 15:21:03 -0400

    debugging

--- networks/english/model.py
+++ networks/english/model.py
@@ -35,10 +35,10 @@ def forward_propagation(images, mnist, nummatches, train=False, dropout=False):
         softmax_layer(5, name="class-softmax-layer")
     ])
     specs = tf.concat([images]*10,0)
-    print("specs",specs.get_shape())
+    # print("specs",specs.get_shape())
     t1 = image_network(mnist)[0]
     t2 = audio_network(specs)[0]
-    embeddings = tf.concat([t1,t2],0)
+    embeddings = tf.concat([t1,t2],1)
     # print("embeddings",embeddings.get_shape())
     _, logits, proba, prediction = classification_network(embeddings)
 
