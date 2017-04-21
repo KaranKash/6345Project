e99a530eb5aa9d0e5e6f84ec6522b56940d6a3cd
Karan Kashyap <kashkar@mit.edu>
2017-04-21 15:15:50 -0400

    updated concat syntax

--- networks/english/model.py
+++ networks/english/model.py
@@ -34,11 +34,11 @@ def forward_propagation(images, mnist, nummatches, train=False, dropout=False):
         fully_connected_layer(512, keep_prob=1.0, name="class-local2-layer"),
         softmax_layer(5, name="class-softmax-layer")
     ])
-    specs = tf.concat(concat_dim=0,values=[images]*10)
+    specs = tf.concat([images]*10,0)
     # print("specs",specs.get_shape())
     t1 = image_network(mnist)[0]
     t2 = audio_network(specs)[0]
-    embeddings = tf.concat(concat_dim=1, values=[t1,t2])
+    embeddings = tf.concat([t1,t2],0)
     # print("embeddings",embeddings.get_shape())
     _, logits, proba, prediction = classification_network(embeddings)
 
