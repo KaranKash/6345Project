fixing softmax

networks/english/nn.py
@@ -63,9 +63,19 @@ def softmax_layer(classes, name='softmax-layer'):
         with tf.variable_scope(name, values=[input_to_layer]):
             with tf.name_scope('weights'):
                 fanin = input_to_layer.get_shape()[1]
-                weights = weight_variable([fanin, classes])
+                try:
+                    weights = weight_variable([fanin, classes])
+                    variable_summaries(weights)
+                except ValueError:
+                    scope.reuse_variables()
+                    weights = weight_variable([fanin, classes])
             with tf.name_scope('biases'):
-                bias = bias_variable(classes)
+                try:
+                    bias = bias_variable(classes)
+                    variable_summaries(bias)
+                except ValueError:
+                    scope.reuse_variables()
+                    bias = bias_variable(classes)
             with tf.name_scope('preactivation'):
                 preactivation = tf.matmul(input_to_layer, weights) + bias
                 logits = preactivation